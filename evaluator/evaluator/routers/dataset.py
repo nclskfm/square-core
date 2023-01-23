1ec31e2b50075a18cc8a7d7556630ad404291e1b
coretta <melicoretta@yahoo.fr>
2023-01-23 10:12:19 +0100

    update post, put and delete for huggingface donwload and get_dataset

--- evaluator/evaluator/routers/dataset.py
+++ evaluator/evaluator/routers/dataset.py
@@ -15,7 +15,6 @@ from evaluator.routers.evaluator import get_dataset_metadata
 
 logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/dataset")
-dataset_item_type = "dataset"
 auth = Auth()
 
 
@@ -57,18 +56,49 @@ async def create_dataset(
         },
     ),
 ):
-    logger.debug(f"post dataset: {dataset}")
+    # check if the skill-type is not empty
+    # download the dataset and check if the dataset_name skill-type and metric exist on huggingface
 
-    # Check if the dataset exist on the database, when yes, the item muss not be added
+    dataset_handler = DatasetHandler
+    dataset_downloaded = dataset_handler.download_dataset(dataset.dataset_name)
+
+    if dataset_downloaded.name != dataset.dataset_name:
+        raise HTTPException(
+            400,
+            f"skill-type of dataset: {dataset.dataset_name} does not exist on the huggingface ",
+        )
+    # check if the skill-type of dataset exist on huggingface
+
+    if dataset_downloaded.skill_type != dataset.skill_type:
+        raise HTTPException(
+            400,
+            f"skill_type of dataset: {dataset.dataset_name} does not exist on the huggingface ",
+        )
+    # check if the metric of dataset exist on huggingface
+    if dataset_downloaded.metric != dataset.metric:
+        raise HTTPException(
+            400,
+            f"metric of dataset: {dataset.dataset_name} does not exist on the huggingface ",
+        )
+
+        # Check if the dataset exist on the database, when yes, the item muss not be added
     if mongo_client.client.evaluator.datasets.count_documents(
         {"dataset_name": dataset.dataset_name}
     ):
-        logger.debug(f"The dataset_name exist on the database!")
-        return {f"dataset: {dataset.dataset_name} exist on the database "}
+        raise HTTPException(
+            200, f"dataset: {dataset.dataset_name} exist on the database "
+        )
 
     else:
+
         mongo_client.client.evaluator.datasets.insert_one(dataset.mongo())
-        return {f"Dataset {dataset.dataset_name} has been added to mongo DB!"}
+        # Download the dataset from
+        dataset_handler = DatasetHandler
+        dataset_handler.get_dataset(dataset.dataset_name)
+
+        logger.info(f"Dataset {dataset.dataset_name} has been added to the database.")
+        m = f"Dataset {dataset.dataset_name} has been added to mongo DB!"
+        raise HTTPException(200, m)
 
 
 @router.get("/{dataset_name}", status_code=200)
@@ -78,25 +108,24 @@ async def get_dataset(
     # get the database
     # get collection and find dataset_name on the collection
 
-    logger.debug(f"GET dataset: dataset_name= {dataset_name}")
     try:
         # check if the item exist
         if mongo_client.client.evaluator.datasets.count_documents(
             {"dataset_name": dataset_name}
         ):
-            logger.debug(f"Dataset_name exist on datasets collection!")
             dataset = Dataset.from_mongo(
                 mongo_client.client.evaluator.datasets.find_one(
                     {"dataset_name": dataset_name}
                 )
             )
-            return {"get dataset: ": dataset}
+            raise HTTPException(200, f"get dataset: {dataset}")
+
         else:
-            return "Dataset_name not exist on the datasets collection!"
+            m = f"Dataset_name: {dataset_name} not exist on the database collection!"
+            raise HTTPException(400, m)
 
     except ValueError as e:
         msg = f"Dataset_name: {dataset_name} not found; Error: {e}"
-        logger.debug(msg)
         raise HTTPException(404, msg)
 
 
@@ -105,24 +134,54 @@ async def get_dataset(
     status_code=200,
 )
 async def update_dataset(dataset_name: str, skill_type: str, metric: str):
-    logger.debug("put dataset")
     # check if the dataset_name exist on the collection mongodb
     try:
+        # check if the skill-type is not empty
+        # download the dataset and check if the dataset_name skill-type and metric exist on huggingface
+
+        dataset_handler = DatasetHandler
+        dataset_downloaded = dataset_handler.download_dataset(dataset.dataset_name)
+
+        if dataset_downloaded.name != dataset.dataset_name:
+            raise HTTPException(
+                400,
+                f"skill-type of dataset: {dataset.dataset_name} does not exist on the huggingface ",
+            )
+        # check if the skill-type of dataset exist on huggingface
+
+        if dataset_downloaded.skill_type != dataset.skill_type:
+            raise HTTPException(
+                400,
+                f"skill_type of dataset: {dataset.dataset_name} does not exist on the huggingface ",
+            )
+        # check if the metric of dataset exist on huggingface
+        if dataset_downloaded.metric != dataset.metric:
+            raise HTTPException(
+                400,
+                f"metric of dataset: {dataset.dataset_name} does not exist on the huggingface ",
+            )
+
         # check if the dataset_name exist
         if mongo_client.client.evaluator.datasets.find_one(
             {"dataset_name": dataset_name}
         ):
-            logger.debug(f"dataset exist")
             myquery = {"dataset_name": dataset_name}
             new_value = {"$set": {"skill-type": skill_type, "metric": metric}}
-            mongo_client.client.evaluator.datasets.update_one(myquery, new_value)
+            mongo_client.client.evaluator.datasets.replace_one(myquery, new_value)
 
-            logger.debug(f"Dataset_name {dataset_name} is updated from mongodb!")
-            return f"Dataset_name: {dataset_name} has be updated"
+            # download the dataset from hugging_face
+            dataset_handler = DatasetHandler
+            dataset_handler.get_dataset(dataset_name)
+            logger.info(
+                f"Dataset_name {dataset_name} has been updated on the database."
+            )
+
+            m = f"Dataset_name: {dataset_name} has be updated"
+            raise HTTPException(200, m)
+
     except ValueError as e:
         msg = f" The dataset_name: {dataset_name} cannot be updated; Error: {e}!"
-        logger.error(msg)
-        raise HTTPException(400, m)
+        raise HTTPException(400, msg)
 
     return dataset_name
 
@@ -132,26 +191,28 @@ async def update_dataset(dataset_name: str, skill_type: str, metric: str):
     status_code=200,
 )
 async def delete_dataset(dataset_name: str):
-    logger.debug("Delete dataset_name on mongodb")
 
     # check if dataset_name exist on the database
     try:
-        logger.debug(f"dataset_name: {dataset_name} exist on the database")
         if mongo_client.client.evaluator.datasets.count_documents(
             {"dataset_name": dataset_name}
         ):
+            # delete dataset from festplatte and remove to the database
+            dataset_handler = DatasetHandler
+            dataset_handler.remove_dataset(dataset_name)
 
             mongo_client.client.evaluator.datasets.delete_one(
                 {"dataset_name": dataset_name}
             )
-            logger.debug(f"Dataset_name {dataset_name} is deleted on the database!")
-            return f" dataset_name {dataset_name} has been deleted on the database"
+            raise HTTPException(
+                200, f"Dataset {dataset_name} has been deleted on the database"
+            )
 
         else:
             logger.debug(f" Dataset_name not exist")
     except ValueError as e:
-        msg = f"Datasetr_name: {dataset_name} does not found; Error: {e}"
+        msg = f"Dataset_name: {dataset_name} does not found; Error: {e}"
         logger.error(msg)
         raise HTTPException(404, msg)
 
-    return f"dataset_name: {dataset_name} not exist on the Database!"
+    return f"dataset_name: {dataset_name}"
